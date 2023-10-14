4f1fdc0f48b94d04e5bd0c7ebc8a4a626d74b9b2
Raju6302 <jeripitivijayraj@gmail.com>
2023-10-14 15:26:18 +0530

    project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/cg";
-import logo from "../Assets/VK-removebg-preview.png"
+import logo from "../Assets/VK-logo.png"
 
 import {
   AiFillStar,
